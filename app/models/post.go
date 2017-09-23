c49eaa887a99f2959a6a3e96e5259e7536f19221
Dmitrii Topornin <dtopornin@gmail.com>
2017-09-23 12:35:09 +0300

    Add gin, add gorm, add .gitignore

---
+++ app/models/post.go
@@ -0,0 +1,11 @@
+package model
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+type Post struct {
+	gorm.Model
+	Title string `json:"title" gorm:"size:255"`
+	Body  string `json:"body"`
+}
--- main.go
+++ main.go
@@ -1,4 +1,21 @@
 package main
 
+import (
+	"blogy/app/models"
+	"blogy/app/router"
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"log"
+)
+
 func main() {
+	db, err := gorm.Open("postgres", "host=localhost port=5432 user=blog_user dbname=blog_db sslmode=disable password=qwerty123")
+	if err != nil {
+		log.Fatal(err)
+	}
+	db.AutoMigrate(&model.Post{})
+	r := gin.Default()
+	router.Route(r)
+	r.Run()
 }
